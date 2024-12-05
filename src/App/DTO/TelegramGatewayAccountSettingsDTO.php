6dcea7f07902f66af233e3b3dfa85bca85a89d46
Yehor Herasymchuk <yehor@dotsplatform.com>
2024-12-05 09:46:43 +0100

    Add telegram gateway as verification provider

---
+++ src/App/DTO/TelegramGatewayAccountSettingsDTO.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Description of TelegramAccountSettingsDTO.php
+ * @copyright Copyright (c) DOTSPLATFORM, LLC
+ * @author    Yehor Herasymchuk <yehor@dotsplatform.com>
+ */
+
+namespace Dotsplatform\Verification\DTO;
+
+
+use Dots\Data\DTO;
+
+class TelegramGatewayAccountSettingsDTO extends DTO
+{
+    protected ?string $access_token;
+
+    protected ?string $sender_name;
+
+    protected ?string $url;
+
+    public function getAccessToken(): ?string
+    {
+        return $this->access_token;
+    }
+
+    public function getSenderName(): ?string
+    {
+        return $this->sender_name;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+
+}
--- src/App/VerificationType.php
+++ src/App/VerificationType.php
@@ -11,5 +11,6 @@ abstract class VerificationType
 {
     public const SMS = 'sms';
     public const CALL = 'call';
+    public const TELEGRAM_GATEWAY = 'telegram_gateway';
     public const CALL_LAST_FOUR_DIGITS = 'call-last-four-digits';
 }
